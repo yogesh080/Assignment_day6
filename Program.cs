5dad8a748b3cf1f5b0bb4dbdf172b684bb34925c
Yogesh <yogeshbietjhs@gmail.com>
2022-06-19 22:57:05 +0530

    Fibonacci series

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Fibonacci Series of number is: ");
+            int a = 0;
+            int b = 1;
+            int c;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+            for (int i = 0; i < 10; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+
+            }
         }
     }
 }
